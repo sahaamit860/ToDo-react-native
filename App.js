162d8d91bcfb4d3d0a1097822a31fa00c9b2e800
darkLord <amit.saha@wsegames.com>
2022-01-16 22:37:42 +0530

    vector icon setup for android

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
 import 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import MainStackNavigator from './src/navigations/MainStackNavigator';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    Icon.loadFont();
+    Ionicons.loadFont();
+  }
+
   render() {
     return <MainStackNavigator />;
   }
