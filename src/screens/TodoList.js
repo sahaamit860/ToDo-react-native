d4f0f4632a216dd8de029e71c52393ef1d8ee5ac
darkLord <amit.saha@wsegames.com>
2022-01-29 23:30:13 +0530

    google fonts add + Typography set

--- src/screens/TodoList.js
+++ src/screens/TodoList.js
@@ -19,6 +19,9 @@ import {cloneDeep} from 'lodash';
 
 import AddFloatingButton from '../components/common/AddFloatingButton';
 import {COLORS} from '../components/shared/colors';
+import Typography from '../components/shared/Typography';
+
+const moment = require('moment');
 
 export default function TodoList(props) {
   const [taskList, setTaskList] = useState([]);
@@ -41,10 +44,12 @@ export default function TodoList(props) {
     let tempTaskList = cloneDeep(taskList);
     tempTaskList.splice(index, 1);
     setTaskList(tempTaskList);
-    AsyncStorage.setItem('taskList', JSON.stringify(tempTaskList.reverse()));
+    setTimeout(() => {
+      AsyncStorage.setItem('taskList', JSON.stringify(tempTaskList.reverse()));
+    }, 200);
   };
 
-  renderListItem = (item, index) => {
+  const renderListItem = (item, index) => {
     return (
       <Animated.View
         style={[styles.listCard]}
@@ -53,7 +58,13 @@ export default function TodoList(props) {
           .springify()}
         exiting={LightSpeedOutRight.duration(500).springify()}
         layout={Layout.springify()}>
-        <Text>{item.taskName}</Text>
+        <View>
+          <Text style={[Typography.Button_Lead]}>{item.taskName}</Text>
+          <Text style={[Typography.Note2, {color: COLORS.grey_400}]}>
+            created on{' '}
+            {moment.unix(item.createTime / 1000).format('Do MMM YYYY')}
+          </Text>
+        </View>
 
         <TouchableOpacity onPress={() => removeList(index)} style={styles.bin}>
           <Ionicons name="trash-outline" size={18} color={COLORS.orange} />
@@ -64,9 +75,7 @@ export default function TodoList(props) {
 
   return (
     <SafeAreaView style={{flex: 1}}>
-      <Text style={{alignSelf: 'center', marginTop: 15}}>All Tasks</Text>
-
-      <Animated.View layout={Layout.springify()}>
+      <Animated.View style={{marginTop: 20}} layout={Layout.springify()}>
         <FlatList
           refreshControl={
             <RefreshControl refreshing={refreshing} onRefresh={getList} />
@@ -94,10 +103,19 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     alignItems: 'center',
     justifyContent: 'space-between',
-    marginTop: 10,
+    marginVertical: 6,
     paddingVertical: 5,
     paddingHorizontal: 15,
     borderRadius: 10,
+    shadowColor: COLORS.black,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+
+    elevation: 3,
   },
   bin: {
     height: 30,
